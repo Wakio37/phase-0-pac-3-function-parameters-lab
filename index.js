5d42dc12917639a7a40944a4fbb95ef09904a936
+ <kingoriperis@gmail.com>
2023-07-13 12:22:31 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+}
+    result = introduction("Aki")
+    console.log(result);
+
+
+
+function introductionWithLanguage(name, language){
+    return`Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+    result = introductionWithLanguage("Aki", "Ember.js")
+    console.log(result);
+
+
+
+function introductionWithLanguageOptional(name, language){
+    return`Hi, my name is ${name} and I am learning to program in ${language}.`;
+
+    }
+
+    result = introductionWithLanguageOptional("Gracie", "JavaScript")
+    console.log(result);
+
+
+function introductionWithLanguageOptional(name, language){
+    return`Hi, my name is ${name} and I am learning to program in ${language}.`;
+    }
+        result = introductionWithLanguageOptional("Gracie", "Python")
+        console.log(result);
+    
+    
+    
